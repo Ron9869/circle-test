c285bdc89959ae2f46a369e7d9e13242eaf7d516
Roman Biryulkin <ramon9869@gmail.com>
2018-03-06 11:42:05 +0500

    sdf

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,13 @@ cat <<EOF >> ~/.ssh/known_hosts
 172.31.96.208 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHHQdkO+/aULBMZyVpR3mk+2mU1OU9Ljf6RN/4qse3aoXqPA2ZlIpBjtwSL5mjr74bLZVJY+bWMZu+NXXeN4lSo=
 EOF
 
-ssh -o ProxyCommand="ssh -A -q -W %h:%p circleci@34.207.40.58" circleci@172.31.96.208 docker service ls
+ssh -o ProxyCommand="ssh -A -q -W %h:%p circleci@34.207.40.58" \
+    "circleci@172.31.96.208 docker service ls \
+    network_created='$(docker network ls --filter name=koyfind --quiet)' \
+    if [[ -z "${network_created}" ]] \
+        docker network create --driver overlay koyfin \
+    fi \"
+
+
 
 cat ~/.ssh/known_hosts
